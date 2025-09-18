edb4b4c898222dd52dfb534fb92c94c6430eddc8
JoseEdSH <josedu1403@gmail.com>
2025-09-18 00:09:07 -0600

    Add files via upload

---
+++ Cuenta.java
@@ -0,0 +1,87 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.DateFormat;
+
+public class Cuenta {
+    private String codCuenta = "cta-";
+    private double saldo;
+    private String nombreCuentaHabiente;
+    private String fechaCreacion;
+    private int cantDepositosRealizados;
+    private int cantRetirosExitososRealizados;
+    private static int cantCuentasCreadas = 0;
+    
+    public Cuenta(String nombreCuentaHabiente, double pSaldo) {
+        this.nombreCuentaHabiente = nombreCuentaHabiente;
+        saldo = pSaldo;
+        
+        fechaCreacion = determinarFechaCreacion();
+        
+        cantDepositosRealizados = 0;
+        cantRetirosExitososRealizados = 0;
+        
+        cantCuentasCreadas ++;
+        codCuenta += cantCuentasCreadas;
+    }
+    
+    public Cuenta(double pSaldo) {
+        this("", pSaldo);
+    }
+    
+    public void setNombreCuentaHabiente(String pNombreCuentaHabiente) {
+        nombreCuentaHabiente = pNombreCuentaHabiente;
+    }
+    
+    public String getCodCuenta() {
+        return codCuenta;
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public void depositar(double monto) {
+        cantDepositosRealizados ++;
+        saldo += monto;
+    }
+    
+    public double retirar(double monto) {
+        if(validarRetiro(monto)){
+            cantRetirosExitososRealizados ++;
+            saldo -= monto;
+        }
+        return saldo;
+    }
+    
+    private boolean validarRetiro(double monto) {
+        if(saldo >= monto)
+            return true;
+        else
+            return false;
+    }
+    
+    public static int getCantCuentasCreadas() {
+        return cantCuentasCreadas;
+    }
+    
+    public String toString() {
+        String mensaje = "";
+        
+        mensaje += "========== Ficha de la Cuenta ==========\n";
+        mensaje += "Código de cuenta: " + codCuenta + "\n";
+        mensaje += "Nombre de la cuenta Habiente: " + nombreCuentaHabiente + "\n";
+        mensaje += "Fecha de creación: " + fechaCreacion + "\n";
+        mensaje += "Saldo en cuenta: " + saldo + "\n";
+        mensaje += "Depósitos realizados: " + cantDepositosRealizados + "\n";
+        mensaje += "Retiros realizados: " + cantRetirosExitososRealizados + "\n";
+        mensaje += "=====================================\n\n";
+        
+        return mensaje;
+    }
+    
+    private String determinarFechaCreacion() {
+        Date fecha = new Date(System.currentTimeMillis());
+        DateFormat formatoFecha = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        return formatoFecha.format(fecha);
+    }
+}
